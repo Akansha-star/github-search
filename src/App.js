54a4c102a49dd312f968f8172d2a6a3d9fa4e1dd
Akansha-star <sendtoakansha@gmail.com>
2021-09-06 12:54:02 +0200

    Added sidebar.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./App.css";
 import Container from "./components/Container";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   const [showMenu, setshowMenu] = React.useState(false);
 
   return (
     <div className="d-flex flex-row">
+      <button
+        onClick={() => setshowMenu(!showMenu)}
+        className="d-sm-block d-md-none menu-button"
+      >
+        |||
+      </button>
+      <div
+        className={`col-md-3s p-md-3 menu d-md-block ${
+          showMenu ? "d-block" : "d-none"
+        }`}
+      >
+        <Sidebar />
+      </div>
       <div className="col-md-9 col-12 container">
         <Container />
       </div>
